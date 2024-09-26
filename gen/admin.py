53f28d9f98d9d56e2a078d54c9574804c42f8217
Joachim Oforma, Igwebuike <joachimigwebuike@gmail.com>
2024-09-26 15:13:15 -0800

    Updating settings.py

--- gen/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-# Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email']
-    search_fields = ['email', 'first_name', 'last_name']
